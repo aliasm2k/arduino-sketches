c84ff7ac18e1ace8ed7564854407a24aba0865d2
Karthik Narendran <aliasm2k@yahoo.in>
2018-06-14 16:13:32 +0530

    Reset Template

---
+++ Reset/Reset.ino
@@ -0,0 +1,16 @@
+/*
+ * Audrino Reset
+ * Template for Audrino programs
+ */
+
+/*
+ * Initialization block.
+ * Runs only once during startup
+ */
+void setup() {}
+
+/*
+ * Operation to be performed
+ * Executes forever
+ */
+void loop() {}
